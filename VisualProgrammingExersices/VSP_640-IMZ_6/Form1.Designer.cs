a3e8521cfe9f03ffef3d1ddda49fd14533d92e1b
Bilyana Dobreva <114009406+bibidobreva@users.noreply.github.com>
2023-07-31 20:00:39 +0300

    Add files via upload

---
+++ VisualProgrammingExersices/VSP_640-IMZ_6/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace VSP_640_IMZ_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Видин",
+            "Лом",
+            "Оряхово",
+            "Свищов",
+            "Русе"});
+            this.checkedListBox1.Location = new System.Drawing.Point(25, 87);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(219, 334);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(359, 92);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(210, 329);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Добави";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(264, 252);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Копирай";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 463);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.MinimumSize = new System.Drawing.Size(629, 502);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ VisualProgrammingExersices/VSP_640-IMZ_7/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace VSP_640_IMZ_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "",
+            ""}, -1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listViewFilesAndFolders = new System.Windows.Forms.ListView();
+            this.imageListLarge = new System.Windows.Forms.ImageList(this.components);
+            this.imageListSmall = new System.Windows.Forms.ImageList(this.components);
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonTie = new System.Windows.Forms.RadioButton();
+            this.radioButtonDetails = new System.Windows.Forms.RadioButton();
+            this.radioButtonList = new System.Windows.Forms.RadioButton();
+            this.radioButtonSmallIcon = new System.Windows.Forms.RadioButton();
+            this.radioButtonLargeIcon = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelCurrentPath = new System.Windows.Forms.Label();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewFilesAndFolders
+            // 
+            this.listViewFilesAndFolders.HideSelection = false;
+            this.listViewFilesAndFolders.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2});
+            this.listViewFilesAndFolders.LargeImageList = this.imageListLarge;
+            this.listViewFilesAndFolders.Location = new System.Drawing.Point(12, 57);
+            this.listViewFilesAndFolders.Name = "listViewFilesAndFolders";
+            this.listViewFilesAndFolders.Size = new System.Drawing.Size(568, 340);
+            this.listViewFilesAndFolders.SmallImageList = this.imageListSmall;
+            this.listViewFilesAndFolders.TabIndex = 0;
+            this.listViewFilesAndFolders.UseCompatibleStateImageBehavior = false;
+            this.listViewFilesAndFolders.View = System.Windows.Forms.View.Details;
+            // 
+            // imageListLarge
+            // 
+            this.imageListLarge.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListLarge.ImageStream")));
+            this.imageListLarge.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListLarge.Images.SetKeyName(0, "Folder.ico");
+            this.imageListLarge.Images.SetKeyName(1, "List.ico");
+            // 
+            // imageListSmall
+            // 
+            this.imageListSmall.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListSmall.ImageStream")));
+            this.imageListSmall.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListSmall.Images.SetKeyName(0, "52.ico");
+            this.imageListSmall.Images.SetKeyName(1, "2.ico");
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(321, 415);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 1;
+            this.buttonBack.Text = "Назад";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonTie);
+            this.groupBox1.Controls.Add(this.radioButtonDetails);
+            this.groupBox1.Controls.Add(this.radioButtonList);
+            this.groupBox1.Controls.Add(this.radioButtonSmallIcon);
+            this.groupBox1.Controls.Add(this.radioButtonLargeIcon);
+            this.groupBox1.Location = new System.Drawing.Point(605, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(183, 340);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Опции";
+            // 
+            // radioButtonTie
+            // 
+            this.radioButtonTie.AutoSize = true;
+            this.radioButtonTie.Location = new System.Drawing.Point(40, 130);
+            this.radioButtonTie.Name = "radioButtonTie";
+            this.radioButtonTie.Size = new System.Drawing.Size(115, 17);
+            this.radioButtonTie.TabIndex = 4;
+            this.radioButtonTie.Text = "Детайлен списък";
+            this.radioButtonTie.UseVisualStyleBackColor = true;
+            this.radioButtonTie.CheckedChanged += new System.EventHandler(this.radioButtonTile_CheckedChanged);
+            // 
+            // radioButtonDetails
+            // 
+            this.radioButtonDetails.AutoSize = true;
+            this.radioButtonDetails.Checked = true;
+            this.radioButtonDetails.Location = new System.Drawing.Point(40, 107);
+            this.radioButtonDetails.Name = "radioButtonDetails";
+            this.radioButtonDetails.Size = new System.Drawing.Size(75, 17);
+            this.radioButtonDetails.TabIndex = 3;
+            this.radioButtonDetails.TabStop = true;
+            this.radioButtonDetails.Text = "Детайлно";
+            this.radioButtonDetails.UseVisualStyleBackColor = true;
+            this.radioButtonDetails.CheckedChanged += new System.EventHandler(this.radioButtonDetails_CheckedChanged);
+            // 
+            // radioButtonList
+            // 
+            this.radioButtonList.AutoSize = true;
+            this.radioButtonList.Location = new System.Drawing.Point(40, 84);
+            this.radioButtonList.Name = "radioButtonList";
+            this.radioButtonList.Size = new System.Drawing.Size(63, 17);
+            this.radioButtonList.TabIndex = 2;
+            this.radioButtonList.Text = "Списък";
+            this.radioButtonList.UseVisualStyleBackColor = true;
+            this.radioButtonList.CheckedChanged += new System.EventHandler(this.radioButtonList_CheckedCHanged);
+            // 
+            // radioButtonSmallIcon
+            // 
+            this.radioButtonSmallIcon.AutoSize = true;
+            this.radioButtonSmallIcon.ImageList = this.imageListSmall;
+            this.radioButtonSmallIcon.Location = new System.Drawing.Point(40, 61);
+            this.radioButtonSmallIcon.Name = "radioButtonSmallIcon";
+            this.radioButtonSmallIcon.Size = new System.Drawing.Size(91, 17);
+            this.radioButtonSmallIcon.TabIndex = 1;
+            this.radioButtonSmallIcon.Text = "Малки икони";
+            this.radioButtonSmallIcon.UseVisualStyleBackColor = true;
+            this.radioButtonSmallIcon.CheckedChanged += new System.EventHandler(this.radioButtonSmallIcon_CheckedChanged);
+            // 
+            // radioButtonLargeIcon
+            // 
+            this.radioButtonLargeIcon.AutoSize = true;
+            this.radioButtonLargeIcon.ImageList = this.imageListLarge;
+            this.radioButtonLargeIcon.Location = new System.Drawing.Point(40, 37);
+            this.radioButtonLargeIcon.Name = "radioButtonLargeIcon";
+            this.radioButtonLargeIcon.Size = new System.Drawing.Size(96, 17);
+            this.radioButtonLargeIcon.TabIndex = 0;
+            this.radioButtonLargeIcon.Text = "Големи икони";
+            this.radioButtonLargeIcon.UseVisualStyleBackColor = true;
+            this.radioButtonLargeIcon.CheckedChanged += new System.EventHandler(this.radioButtonLargeIcon_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Текуща директория:";
+            // 
+            // labelCurrentPath
+            // 
+            this.labelCurrentPath.AutoSize = true;
+            this.labelCurrentPath.Location = new System.Drawing.Point(129, 22);
+            this.labelCurrentPath.Name = "labelCurrentPath";
+            this.labelCurrentPath.Size = new System.Drawing.Size(35, 13);
+            this.labelCurrentPath.TabIndex = 5;
+            this.labelCurrentPath.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelCurrentPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.listViewFilesAndFolders);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Изобразяване на файлове и папки";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+     
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewFilesAndFolders;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonTie;
+        private System.Windows.Forms.RadioButton radioButtonDetails;
+        private System.Windows.Forms.RadioButton radioButtonList;
+        private System.Windows.Forms.RadioButton radioButtonSmallIcon;
+        private System.Windows.Forms.RadioButton radioButtonLargeIcon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList imageListSmall;
+        private System.Windows.Forms.ImageList imageListLarge;
+        private System.Windows.Forms.Label labelCurrentPath;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
