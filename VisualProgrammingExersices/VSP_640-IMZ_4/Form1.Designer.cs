f8afbf3d5334b350babde3ac9859c7b3f91d713c
Bilyana Dobreva <114009406+bibidobreva@users.noreply.github.com>
2023-07-31 19:59:36 +0300

    Add files via upload

---
+++ VisualProgrammingExersices/VSP_640-IMZ_4/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace VSP_640_IMZ_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelAdress = new System.Windows.Forms.Label();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.labelResults = new System.Windows.Forms.Label();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxAdress = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.checkBoxProgrammer = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonFemale = new System.Windows.Forms.RadioButton();
+            this.radioButtonMale = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(61, 52);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(29, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Име";
+            // 
+            // labelAdress
+            // 
+            this.labelAdress.AutoSize = true;
+            this.labelAdress.Location = new System.Drawing.Point(64, 112);
+            this.labelAdress.Name = "labelAdress";
+            this.labelAdress.Size = new System.Drawing.Size(38, 13);
+            this.labelAdress.TabIndex = 1;
+            this.labelAdress.Text = "Адрес";
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Location = new System.Drawing.Point(48, 418);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(50, 13);
+            this.labelAge.TabIndex = 3;
+            this.labelAge.Text = "Възраст";
+            // 
+            // labelResults
+            // 
+            this.labelResults.AutoSize = true;
+            this.labelResults.Location = new System.Drawing.Point(48, 450);
+            this.labelResults.Name = "labelResults";
+            this.labelResults.Size = new System.Drawing.Size(59, 13);
+            this.labelResults.TabIndex = 4;
+            this.labelResults.Text = "Резултати";
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOk.Location = new System.Drawing.Point(527, 42);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 5;
+            this.buttonOk.Text = "ОК";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // buttonHelp
+            // 
+            this.buttonHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonHelp.CausesValidation = false;
+            this.buttonHelp.Location = new System.Drawing.Point(527, 71);
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.Size = new System.Drawing.Size(75, 23);
+            this.buttonHelp.TabIndex = 6;
+            this.buttonHelp.Text = "Помощ";
+            this.buttonHelp.UseVisualStyleBackColor = true;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxName.Location = new System.Drawing.Point(135, 52);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(327, 20);
+            this.textBoxName.TabIndex = 7;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // textBoxAdress
+            // 
+            this.textBoxAdress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAdress.Location = new System.Drawing.Point(135, 105);
+            this.textBoxAdress.Multiline = true;
+            this.textBoxAdress.Name = "textBoxAdress";
+            this.textBoxAdress.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxAdress.Size = new System.Drawing.Size(327, 100);
+            this.textBoxAdress.TabIndex = 8;
+            this.textBoxAdress.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxOutput.Location = new System.Drawing.Point(51, 492);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(551, 219);
+            this.textBoxOutput.TabIndex = 9;
+            this.textBoxOutput.Text = "";
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAge.Location = new System.Drawing.Point(135, 411);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(327, 20);
+            this.textBoxAge.TabIndex = 10;
+            this.textBoxAge.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxAge_KeyDown);
+            this.textBoxAge.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxAge_KeyPress);
+            this.textBoxAge.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxAge_KeyUp);
+            // 
+            // checkBoxProgrammer
+            // 
+            this.checkBoxProgrammer.AutoSize = true;
+            this.checkBoxProgrammer.Location = new System.Drawing.Point(67, 241);
+            this.checkBoxProgrammer.Name = "checkBoxProgrammer";
+            this.checkBoxProgrammer.Size = new System.Drawing.Size(88, 17);
+            this.checkBoxProgrammer.TabIndex = 11;
+            this.checkBoxProgrammer.Text = "Програмист";
+            this.checkBoxProgrammer.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonFemale);
+            this.groupBox1.Controls.Add(this.radioButtonMale);
+            this.groupBox1.Location = new System.Drawing.Point(64, 291);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(314, 100);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Пол";
+            // 
+            // radioButtonFemale
+            // 
+            this.radioButtonFemale.AutoSize = true;
+            this.radioButtonFemale.Location = new System.Drawing.Point(31, 57);
+            this.radioButtonFemale.Name = "radioButtonFemale";
+            this.radioButtonFemale.Size = new System.Drawing.Size(54, 17);
+            this.radioButtonFemale.TabIndex = 1;
+            this.radioButtonFemale.TabStop = true;
+            this.radioButtonFemale.Text = "Жена";
+            this.radioButtonFemale.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMale
+            // 
+            this.radioButtonMale.AutoSize = true;
+            this.radioButtonMale.Location = new System.Drawing.Point(31, 19);
+            this.radioButtonMale.Name = "radioButtonMale";
+            this.radioButtonMale.Size = new System.Drawing.Size(49, 17);
+            this.radioButtonMale.TabIndex = 0;
+            this.radioButtonMale.TabStop = true;
+            this.radioButtonMale.Text = "Мъж";
+            this.radioButtonMale.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 774);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBoxProgrammer);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxAdress);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.buttonHelp);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.labelResults);
+            this.Controls.Add(this.labelAge);
+            this.Controls.Add(this.labelAdress);
+            this.Controls.Add(this.labelName);
+            this.MinimumSize = new System.Drawing.Size(667, 813);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelAdress;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.Label labelResults;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonHelp;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxAdress;
+        private System.Windows.Forms.RichTextBox textBoxOutput;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.CheckBox checkBoxProgrammer;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonFemale;
+        private System.Windows.Forms.RadioButton radioButtonMale;
+    }
+}
+
---
+++ VisualProgrammingExersices/VSP_640-IMZ_4/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VSP_640_IMZ_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.buttonOk.Enabled = false;
+
+            this.textBoxAdress.Tag = false;
+            this.textBoxAge.Tag = false;
+            this.textBoxName.Tag = false;
+            
+
+            this.textBoxName.Validating += new CancelEventHandler(texBoxEmpty_Validating);
+            this.textBoxAdress.Validating += new CancelEventHandler(texBoxEmpty_Validating);
+           
+            this.textBoxAge.Validating += new CancelEventHandler(this.texBoxEmpty_Validating);
+            this.textBoxAge.Validating += new CancelEventHandler(this.textBoxAge_Validating);
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            string output;
+            output = "Име: " + this.textBoxName.Text + "\r\n";
+            output += "Адрес: " + this.textBoxAdress.Text + "\r\n";
+            output += "Професия: "
+                + (string)(this.checkBoxProgrammer.Checked
+                ? "Програмист"
+                : "Не е програмист")
+                + "\r\n";
+            output += "Пол: " +
+                (string)(this.radioButtonMale.Checked ? "Мъж" : "Жена") + "\r\n";
+            output += "Възраст " + this.textBoxAge.Text;
+            this.textBoxOutput.Text = output;
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            string output;
+            output = "Помощна информация: \r\n\r\n";
+            output += "Име = Вашето име \r\n";
+            output += "Адрес = Вашия адрес \r\n ";
+            output += "Програмист = Маркирайте полето, ако сте програмист\r\n";
+            output += "Пол = Маркирайте своя пол\r\n";
+            output += "Възраст = възраст";
+            this.textBoxOutput.Text = output;
+        }
+        private void ValidateOK()
+        {
+            this.buttonOk.Enabled = (
+                    (bool)(this.textBoxAdress.Tag) &&
+                    (bool)(this.textBoxAge.Tag) &&
+                    (bool)(this.textBoxName.Tag) 
+                );
+        }
+        private void texBoxEmpty_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text.Length == 0)
+            {
+                tb.BackColor = Color.Red;
+                tb.Tag = false;
+            }
+            else
+            {
+                tb.BackColor = System.Drawing.SystemColors.Window;
+                tb.Tag = true;
+            }
+            ValidateOK();
+        }
+       
+
+        private void textBoxAge_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (((tb.Text.Length == 0) && (e.KeyChar == 48)) || ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8))
+            {
+                e.Handled = true;
+            }
+        }
+        private void textBoxAge_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text.Length > 0)
+            {
+                if (Int16.Parse(tb.Text.ToString()) < 18)
+                {
+                    tb.Tag = false;
+                    tb.BackColor = Color.Red;
+                }
+                else
+                {
+                    tb.Tag = true;
+                    tb.BackColor = SystemColors.Window;
+                }
+                ValidateOK();
+            }
+        }
+        private void textBoxAge_KeyUp(object sender, KeyEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text.Length > 0)
+            {
+                if (Int16.Parse(tb.Text.ToString()) < 18)
+                {
+                    tb.Tag = false;
+                    tb.BackColor = Color.Red;
+                }
+                else
+                {
+                    tb.Tag = true;
+                    tb.BackColor = SystemColors.Window;
+                }
+                ValidateOK();
+            }
+        }
+        private void textBoxAge_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text.Length == 0 )
+            {
+                tb.Tag = false;
+                tb.BackColor = Color.Red;
+            }
+            else
+            {
+                tb.Tag = true;
+                tb.BackColor = SystemColors.Window;
+            }
+            ValidateOK();
+        }
+    }
+}
