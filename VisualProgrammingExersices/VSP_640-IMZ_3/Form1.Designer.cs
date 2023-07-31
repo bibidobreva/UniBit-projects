bd08a4a92dc9e5231bccf4c2264d755ce4f92a0b
Bilyana Dobreva <114009406+bibidobreva@users.noreply.github.com>
2023-07-31 19:59:15 +0300

    Add files via upload

---
+++ VisualProgrammingExersices/VSP_640-IMZ_3/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace VSP_640_IMZ_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelAdress = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxAdress = new System.Windows.Forms.TextBox();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.labelProfession = new System.Windows.Forms.Label();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.labelResults = new System.Windows.Forms.Label();
+            this.textBoxProfession = new System.Windows.Forms.TextBox();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(47, 36);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(29, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Име";
+            this.labelName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelAdress
+            // 
+            this.labelAdress.AutoSize = true;
+            this.labelAdress.Location = new System.Drawing.Point(47, 94);
+            this.labelAdress.Name = "labelAdress";
+            this.labelAdress.Size = new System.Drawing.Size(38, 13);
+            this.labelAdress.TabIndex = 1;
+            this.labelAdress.Text = "Адрес";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxName.Location = new System.Drawing.Point(143, 33);
+            this.textBoxName.Multiline = true;
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(384, 20);
+            this.textBoxName.TabIndex = 2;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            this.textBoxName.Validating += new System.ComponentModel.CancelEventHandler(this.texBoxEmpty_Validating);
+            // 
+            // textBoxAdress
+            // 
+            this.textBoxAdress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAdress.Location = new System.Drawing.Point(143, 94);
+            this.textBoxAdress.Multiline = true;
+            this.textBoxAdress.Name = "textBoxAdress";
+            this.textBoxAdress.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxAdress.Size = new System.Drawing.Size(384, 79);
+            this.textBoxAdress.TabIndex = 3;
+            this.textBoxAdress.Validating += new System.ComponentModel.CancelEventHandler(this.texBoxEmpty_Validating);
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOk.Location = new System.Drawing.Point(590, 36);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 4;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // buttonHelp
+            // 
+            this.buttonHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonHelp.CausesValidation = false;
+            this.buttonHelp.Location = new System.Drawing.Point(590, 75);
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.Size = new System.Drawing.Size(75, 23);
+            this.buttonHelp.TabIndex = 5;
+            this.buttonHelp.Text = "Help";
+            this.buttonHelp.UseVisualStyleBackColor = true;
+            this.buttonHelp.Click += new System.EventHandler(this.buttonHelp_Click);
+            // 
+            // labelProfession
+            // 
+            this.labelProfession.AutoSize = true;
+            this.labelProfession.Location = new System.Drawing.Point(34, 188);
+            this.labelProfession.Name = "labelProfession";
+            this.labelProfession.Size = new System.Drawing.Size(59, 13);
+            this.labelProfession.TabIndex = 6;
+            this.labelProfession.Text = "Професия";
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Location = new System.Drawing.Point(34, 239);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(50, 13);
+            this.labelAge.TabIndex = 7;
+            this.labelAge.Text = "Възраст";
+            // 
+            // labelResults
+            // 
+            this.labelResults.AutoSize = true;
+            this.labelResults.Location = new System.Drawing.Point(34, 287);
+            this.labelResults.Name = "labelResults";
+            this.labelResults.Size = new System.Drawing.Size(59, 13);
+            this.labelResults.TabIndex = 8;
+            this.labelResults.Text = "Резултати";
+            this.labelResults.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBoxProfession
+            // 
+            this.textBoxProfession.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxProfession.Location = new System.Drawing.Point(143, 188);
+            this.textBoxProfession.Name = "textBoxProfession";
+            this.textBoxProfession.Size = new System.Drawing.Size(384, 20);
+            this.textBoxProfession.TabIndex = 9;
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAge.Location = new System.Drawing.Point(143, 239);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(156, 20);
+            this.textBoxAge.TabIndex = 10;
+            this.textBoxAge.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxAge_KeyDown);
+            this.textBoxAge.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxAge_KeyPress);
+            this.textBoxAge.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxAge_KeyUp);
+            this.textBoxAge.Validating += new System.ComponentModel.CancelEventHandler(this.texBoxEmpty_Validating);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxOutput.Location = new System.Drawing.Point(37, 330);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(490, 206);
+            this.textBoxOutput.TabIndex = 11;
+            this.textBoxOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 589);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.textBoxProfession);
+            this.Controls.Add(this.labelResults);
+            this.Controls.Add(this.labelAge);
+            this.Controls.Add(this.labelProfession);
+            this.Controls.Add(this.buttonHelp);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.textBoxAdress);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelAdress);
+            this.Controls.Add(this.labelName);
+            this.MinimumSize = new System.Drawing.Size(708, 628);
+            this.Name = "Form1";
+            this.Text = "Въвеждане на лични данни";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelAdress;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxAdress;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonHelp;
+        private System.Windows.Forms.Label labelProfession;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.Label labelResults;
+        private System.Windows.Forms.TextBox textBoxProfession;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.RichTextBox textBoxOutput;
+    }
+}
+
---
+++ VisualProgrammingExersices/VSP_640-IMZ_3/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VSP_640_IMZ_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.buttonOk.Enabled = false;
+
+            this.textBoxAdress.Tag = false;
+            this.textBoxAge.Tag = false;
+            this.textBoxName.Tag = false;
+            this.textBoxProfession.Tag = false;
+
+            this.textBoxName.Validating += new CancelEventHandler(texBoxEmpty_Validating);
+            this.textBoxAdress.Validating += new CancelEventHandler(texBoxEmpty_Validating);
+            this.textBoxProfession.Validating += new CancelEventHandler(textBoxProffesion_Validating);
+            this.textBoxAge.Validating += new CancelEventHandler(this.texBoxEmpty_Validating);
+            this.textBoxAge.Validating += new CancelEventHandler(this.textBoxAge_Validating);
+            
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            string output;
+            output = "Име " + this.textBoxName.Text + "\r\n";
+            output += "Адрес " + this.textBoxAdress.Text + "\r\n";
+            output += "Професия " + this.textBoxProfession.Text + "\r\n";
+            output += "Възраст " + this.textBoxAge.Text;
+            this.textBoxOutput.Text = output;
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            string output;
+            output = "Помощна информация: \r\n\r\n";
+            output += "Име = Вашето име \r\n";
+            output += "Адрес = Вашия адрес \r\n ";
+            output += "Професия = Единствената правилна форма е 'Програмист'\r\n";
+            output += "Възраст = възраст";
+            this.textBoxOutput.Text = output;
+        }
+        private void ValidateOK()
+        {
+            this.buttonOk.Enabled = (
+                    (bool)(this.textBoxAdress.Tag) &&
+                    (bool)(this.textBoxAge.Tag) &&
+                    (bool)(this.textBoxName.Tag) &&
+                    (bool)(this.textBoxProfession.Tag)
+                );
+        }
+        private void texBoxEmpty_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text.Length == 0)
+            {
+                tb.BackColor = Color.Red;
+                tb.Tag = false;
+            }
+            else
+            {
+                tb.BackColor = System.Drawing.SystemColors.Window;
+                tb.Tag = true;
+            }
+            ValidateOK();
+        }
+        private void textBoxProffesion_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text.CompareTo("Програмист") == 0 || tb.Text.Length == 0)
+            {
+                tb.Tag = true;
+                tb.BackColor = System.Drawing.SystemColors.Window;
+            }
+            else
+            {
+                tb.Tag = false;
+                tb.BackColor = Color.Red;
+            }
+            ValidateOK();
+        }
+
+        private void textBoxAge_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if(((tb.Text.Length==0)&&(e.KeyChar==48))||((e.KeyChar<48||e.KeyChar>57)&&e.KeyChar != 8))
+            {
+                e.Handled = true;
+            }
+        }
+        private void textBoxAge_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text.Length > 0)
+            {
+                if (Int16.Parse(tb.Text.ToString()) < 18)
+                {
+                    tb.Tag = false;
+                    tb.BackColor = Color.Red;
+                }
+                else
+                {
+                    tb.Tag = true;
+                    tb.BackColor = SystemColors.Window;
+                }
+                ValidateOK();
+            }
+        }
+        private void textBoxAge_KeyUp(object sender, KeyEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text.Length > 0)
+            {
+                if (Int16.Parse(tb.Text.ToString()) < 18)
+                {
+                    tb.Tag = false;
+                    tb.BackColor = Color.Red;
+                }
+                else
+                {
+                    tb.Tag = true;
+                    tb.BackColor = SystemColors.Window;
+                }
+                ValidateOK();
+            }
+        }
+        private void textBoxAge_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if(tb.Text.Length==0&&tb!= textBoxProfession)
+            {
+                tb.Tag = false;
+                tb.BackColor = Color.Red;
+            }
+            else
+            {
+                tb.Tag = true;
+                tb.BackColor = SystemColors.Window;
+            }
+            ValidateOK();
+        }
+    }
+}
